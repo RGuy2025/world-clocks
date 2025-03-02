e6f1b6d8672a9cf13b5d4b69123b367d152ac892
RGuy2025 <r.s.guyett@gmail.com>
2025-03-02 18:21:06 -0500

    added current city and functionality on drop down menu

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,13 @@
 function updateTime() {
+  //Brantford
+  let brantfordElement = document.querySelector("#brantford");
+  let brantfordDateElement = brantfordElement.querySelector(".date");
+  let brantfordTimeElement = brantfordElement.querySelector(".time");
+  let brantfordTime = moment().tz("America/Toronto");
+  brantfordDateElement.innerHTML = brantfordTime.format("dddd, MMMM Do YYYY");
+  brantfordTimeElement.innerHTML = brantfordTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
   //Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
@@ -18,5 +27,28 @@ function updateTime() {
   tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("ddd, MMMM Do YYYY")}
+          </div>
+        </div>
+        <div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>
+      </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
